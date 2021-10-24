36ded364142303bb8226c1b6d170268ef192836d
ysur67 <rus.muratow@gmail.com>
2021-10-24 19:43:54 +0300

    feat:  fetch uses data from remote

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module main
 
 go 1.17
+
+require github.com/joho/godotenv v1.4.0 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,55 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
+)
+
+type Address struct {
+	Street  string `json:"street"`
+	Suite   string `json:"suite"`
+	City    string `json:"city"`
+	Zipcode string `json:"zipcode"`
+}
+
+type Company struct {
+	Name        string `json:"name"`
+	CatchPhrase string `json:"catchPhrase"`
+	BS          string `json:"bs"`
+}
+
+type User struct {
+	Id       int     `json:"id"`
+	Name     string  `json:"name"`
+	Username string  `json:"username"`
+	Email    string  `json:"email"`
+	Address  Address `json:"address"`
+	Phone    string  `json:"phone"`
+	Website  string  `json:"website"`
+}
+
+func getJsonResponse(url string, output interface{}) {
+	request, err := http.Get(url)
+	if err != nil {
+		panic(err.Error())
+	}
+	decoder := json.NewDecoder(request.Body)
+	err = decoder.Decode(&output)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err.Error())
+	}
+	usersData := []User{}
+	getJsonResponse(os.Getenv("user-source"), &usersData)
+	fmt.Println(usersData)
 }
