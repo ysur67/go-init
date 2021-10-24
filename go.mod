0c6b5dddad7d1fcc68ff7943767e4d121c101f7c
ysur67 <rus.muratow@gmail.com>
2021-10-24 20:25:56 +0300

    feat: connect to postgresql server

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module main
 
 go 1.17
 
-require github.com/joho/godotenv v1.4.0 // indirect
+require (
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/lib/pq v1.10.3 // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type Address struct {
@@ -61,4 +64,24 @@ func main() {
 	var postsUrl = os.Getenv("post-source")
 	getJsonResponse(usersUrl, &usersData)
 	getJsonResponse(postsUrl, &postsData)
+	host := os.Getenv("database-host")
+	port := os.Getenv("database-port")
+	user := os.Getenv("database-user-username")
+	password := os.Getenv("database-user-password")
+	dbname := os.Getenv("database-name")
+
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s "+s
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	fmt.Println(connectionString)
+	connection, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer connection.Close()
+	err = connection.Ping()
+	if err != nil {
+		panic(err)
+	}
+
 }
